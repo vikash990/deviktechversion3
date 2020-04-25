b594e850262ecf15f4c7ba5dfd0c9f070fbde3ed
vikash990 <vikashadubey>
2020-04-25 18:19:31 +0530

    Updates

---
+++ precache-manifest.1890c222840c4b0bb394a3308a3b206f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8548f3a32d2bae9ab0d6bd22ad6e40b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30bcd870ba3aede9b5c7",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0198c87c657b089d0f2c",
+    "url": "/static/css/main.d13c399a.chunk.css"
+  },
+  {
+    "revision": "30bcd870ba3aede9b5c7",
+    "url": "/static/js/2.f2df4323.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f2df4323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0198c87c657b089d0f2c",
+    "url": "/static/js/main.4dc9fe11.chunk.js"
+  },
+  {
+    "revision": "3fcab70e042d81ffea78",
+    "url": "/static/js/runtime-main.0548e3b8.js"
+  },
+  {
+    "revision": "176cc6c05df89554c04d02f2d0a77d96",
+    "url": "/static/media/angle_brackets.176cc6c0.jpg"
+  },
+  {
+    "revision": "5fe1d31f86ef4029cee0e239fff3e57f",
+    "url": "/static/media/deviktech.5fe1d31f.png"
+  },
+  {
+    "revision": "f77da8f1b9bf13c536e8dc220c71a72b",
+    "url": "/static/media/ds_algo_logo.f77da8f1.png"
+  }
+]);
